8bd405b8bfd732189ea861dd69a7afc7c01429fa
steinerr12 <fernandodpaz12@gmail.com>
2026-07-17 23:42:06 -0600

    Update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 
 class Program
@@ -47,21 +48,42 @@ class Program
 
                 int opcion = int.Parse(Console.ReadLine());
                 switch (opcion)
-                {
-                    case 1:
+                {                 
+                    // RETIRAR DINERO
+
+                    case 1: 
                         Console.WriteLine("Ingrese el monto a retirar:");
                         decimal montoARetirar = decimal.Parse(Console.ReadLine());
-                        if (montoARetirar <= limiteDiarioDeRetiro && montoARetirar <= montoMaximoDiario && montoARetirar <= saldo && montoARetirar % 10 == 0)
+
+                       if (montoARetirar <=0)
                         {
-                            saldo -= montoARetirar;
-                            
-                            Console.WriteLine("Retiro exitoso. Su nuevo saldo es: $" + saldo);
+                            Console.WriteLine("El monto a retirar debe ser mayor a cero.");
+                        }
+                        else if (montoARetirar > saldo)
+                        {
+                            Console.WriteLine("Saldo insuficiente para realizar el retiro.");
+                        }
+                        else if (montoARetirar > limiteDiarioDeRetiro)
+                        {
+                            Console.WriteLine("El monto a retirar excede el límite diario de retiro.");
+                        }
+                       else if (montoARetirar > 500.00m)
+                        {
+                            Console.WriteLine("No puede retirar mas de $500.00 en una sola operación");
+                        }
+                       else if (montoARetirar %10 != 0)
+                        {
+                            Console.WriteLine("Solo se permiten multiplos de 10");
                         }
                         else
                         {
-                            Console.WriteLine("Monto de retiro excede el límite diario o su saldo disponible.");
+                            saldo -= montoARetirar;
+                            Console.WriteLine("Retiro exitoso. Su nuevo saldo es: $" + saldo);
+                            Console.WriteLine("Su nuevo saldo es: $" + saldo);
                         }
                         break;
+
+                    // CONSULTAR SALDO
 
                     case 2:
                         Console.WriteLine("Su saldo actual es: $" + saldo);
@@ -69,48 +91,43 @@ class Program
                         Console.WriteLine("Su monto restante para retirar es: $" + (limiteDiarioDeRetiro - saldo));
                         break;
 
-                     case 3:
+                    // DEPOSITAR DINERO
+
+                    case 3:
                         Console.WriteLine("Ingrese el monto a depositar:");
                         decimal montoADepositar = decimal.Parse(Console.ReadLine());
                         saldo += montoADepositar;
                         
-
-                        if (montoADepositar > 2500)
+                        if (montoADepositar <= 0)
                         {
-                            Console.WriteLine("Deposito Sujeto a revisión por exceder el monto máximo permitido.");
-                       
+                            Console.WriteLine("El monto a depositar debe ser mayor a cero.");
                         }
+                        else if (montoADepositar > 5000.00m)
+                        {
+                            Console.WriteLine("No puede depositar un monto mayor a $5,000.00");
+                        }                     
+
+                        else if (montoADepositar > 2500.00m)
+                        {
+                            Console.WriteLine("Depósito sujeto a revisión bancaria.");
+                        }
+
                         else
                         {
+                            saldo += montoADepositar;
                             Console.WriteLine("Depósito exitoso. Su nuevo saldo es: $" + saldo);
                         }
-
                         break;
+
 
                     case 4:
                         Console.WriteLine("Ingrese el número de cuenta del destinatario:");
                         int cuentaDestinatario = int.Parse(Console.ReadLine());
+
                         Console.WriteLine("Ingrese el monto a transferir:");
                         decimal montoATransferir = decimal.Parse(Console.ReadLine());
-                        if (montoATransferir <= saldo)
-                        {
-                            saldo -= montoATransferir;
-                            Console.WriteLine("Transferencia exitosa. Su nuevo saldo es: $" + saldo);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Saldo insuficiente para realizar la transferencia.");
-                        }
-                        if (cuentaDestinatario == 100200300)
-                        {
-                            Console.WriteLine("No puede transferirse a la misma cuenta.");
-                        }
 
-                        break;
 
-                    default:
-                        Console.WriteLine("Opción no válida.");
-                        break;
                 }
                 break;
                 default:
